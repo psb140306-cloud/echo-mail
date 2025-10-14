df653cd15a04698c58b457b975a7d0ced7a378f7
Echo Mail Development Team <dev@echomail.com>
2025-10-15 00:01:15 +0900

    test: Add simple test page without UI components

---
+++ app/test-simple/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>Echo Mail Test</h1>
+      <p>If you see this, the deployment works!</p>
+      <p>Components folder issue resolved: Testing basic deployment first.</p>
+    </div>
+  )
+}
